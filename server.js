d9dc9aae5662d2e5095d0df340f6010be0882392
Efren <efren.gomez0630@gmail.com>
2018-07-26 09:13:42 -0700

    first commit

---
+++ server.js
@@ -0,0 +1,62 @@
+const express = require('express')
+const morgan = require('morgan')
+const bodyParser = require('body-parser')
+const FormData = require('form-data')
+
+const app = express()
+
+app.use(morgan('dev'))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true}))
+app.use(express.static('./styles'))
+
+app.set('views', './views')
+
+app.set('view engine', 'ejs')
+
+app.get('/', (req, res) => {
+    res.render('index')
+})
+
+app.listen(8080, () => {
+    console.log('listening at http://localhost:8080')
+})
+
+app.get('/contact', (req, res) => {
+    res.render('contact')
+  })
+
+  app.get('/about', (req, res) => {
+    res.render('about')
+  })
+  
+  app.post('/thanks', (req, res) => {
+    const url = 'https://script.google.com/macros/s/AKfycbyph7bkSZMh7Rglua-f0ZBHGJcsQnJ6QDbq_CpA5pHx_mbwUpuU/exec';
+    const data = new FormData()
+    const fields = Object.keys(req.body);
+
+    fields.forEach(field => {
+        data.append(field, req.body[field])
+    })
+    data.append('TimeStamp', Date())
+    
+    const config = { headers: { 'Content-Type': 'multipart/form-data' } }
+    data.submit(url, function(err, res) {
+    })
+
+    res.render('thanks', { contact: req.body })
+  })
+
+
+app.get('/', (req, res) => {
+    const data = {
+      person: {
+        firstName: 'Efren',
+        lastName: 'Gomez',
+      }
+    }
+    res.render('index', {
+        firstName: 'Efren',
+        lastName: 'Gomez',
+      })
+})
